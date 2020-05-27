d367fd16a0356cb26b2d7b505b1e98e3b72df8e8
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2020-05-27 13:42:20 +1000

    Update show_go_screen.py

--- show_go_screen.py
+++ show_go_screen.py
@@ -3,12 +3,14 @@ def show_go_screen():
     text_surface = font.render("Press space bar to play again", True, white)
     screen.blit(text_surface, (400, 350))
     pygame.display.flip()
-    done = False
-    while not done:
+    global done
+    again = False
+    while not again:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                again = True
                 done = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    done = True
+                    again = True
         clock.tick(20)
